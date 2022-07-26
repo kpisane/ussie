2fb3062d3e4a83b84a9fd560dbf6fc774587dbca
Kelly Pisane <kellypisane@outlook.com>
2022-07-26 09:24:32 -0400

    adhere to style guide in test package

--- tests/testthat/test-matches.R
+++ tests/testthat/test-matches.R
@@ -1,10 +1,9 @@
 test_that("uss_make_matches works", {
   italy <- uss_make_matches(engsoccerdata::spain, "Italy")
   expect_true(tibble::is_tibble(italy))
-  expect_named(italy, c( "country","tier","season","date","home","visitor",
-                         "goals_home","goals_visitor"))
+  expect_named(italy, c("country", "tier", "season", "date", "home", "visitor",
+                         "goals_home", "goals_visitor"))
   expect_identical(unique(italy$country), "Italy")
   expect_s3_class(italy$tier, "factor")
-  expect_s3_class(italy$date, 'Date')
+  expect_s3_class(italy$date, "Date")
 })
-
